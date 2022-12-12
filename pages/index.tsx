45307798a3637e1be0f6ba3c7ee381e587051ba7
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2022-12-12 17:00:41 +0000

    Updated to make it clean

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,24 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>Studio 42</h1>
-      <span className={styles.span}>🚧 Under construction 🚧</span>
-      <a href="mailto:hello@42.studio">hello@42.studio</a>
-    </main>
+    <div>
+      <main className={styles.main}>
+        <span className={styles.span}>
+          <span>website under construction</span>🚧
+        </span>
+
+        <div className={styles.stack} style={{ ["--stacks" as any]: 3 }}>
+          <span style={{ ["--index" as any]: 0 }}>Studio 42</span>
+          <span style={{ ["--index" as any]: 1 }}>Studio 42</span>
+          <span style={{ ["--index" as any]: 2 }}>Studio 42</span>
+        </div>
+
+        <div className={styles.email}>
+          a web design and development studio, lets work together<br></br>
+          <a href="mailto:hello@42.studio">hello@42.studio</a>
+        </div>
+      </main>
+      <div className={styles.cover}></div>
+    </div>
   );
 }
